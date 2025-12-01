5051f87ef3ebd0263911f257dd9234584b886fee
liam.karv <karvelasliam@gmail.com>
2025-12-01 23:12:14 +1100

    Wrote code for GetInput

--- Calculator/calculator.go
+++ Calculator/calculator.go
@@ -2,9 +2,28 @@ package main
 
 import ("fmt")
 
+func GetInput() (string, int, int) {
+	var operationType string
+	var number1 int
+	var number2 int
 
+	fmt.Println("Choose an operation (+, -, *, /): ")
+	fmt.Scan(&operationType)
 
+	fmt.Println(": ")
+	fmt.Scan(&number1)
 
-func main() {
-	fmt.Println("Ben")
+	fmt.Println(": ")
+	fmt.Scan(&number2)
+	return operationType, number1, number2
+}
+
+func CalculateResult(operation string, n1 int, n2 int) int {
+	
+	
+	return 
+}
+
+func main(){
+	CalculateResult(GetInput())
 }
